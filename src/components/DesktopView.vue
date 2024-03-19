50e83c21d15c5c12ed83da1aa38ace1c6406983f
TheOratorEse <oratoreseiwi@gmail.com>
2024-03-19 21:20:17 +0100

    styling

--- src/components/DesktopView.vue
+++ src/components/DesktopView.vue
@@ -75,10 +75,11 @@ export default {
 /*right section */
 .right-section {
   background-color: #FCFCFC;
+  max-width: 50vmax;
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100%;
+ 
 }
 
 .login-container {
@@ -91,8 +92,8 @@ export default {
 .login-card {
   background-color: #ffffff;
   padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+ /*  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); */
   width: 300px;
 }
 
@@ -124,7 +125,7 @@ button {
 
 .remember-me {
   display: flex;
-  align-items: center;
+  align-items: left;
   margin-bottom: 10px;
 }
 
